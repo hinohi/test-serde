560bcad5c493b88c11d497676992fd013f417b76
Daiju Nakayama <42.daiju@gmail.com>
2018-10-02 22:55:49 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate ron;
+extern crate serde;
+extern crate serde_json;
+extern crate serde_pickle;
+extern crate serde_yaml;
+extern crate toml;
+
+mod val;
+
+fn main() {
+    val::run();
+}
---
+++ src/val.rs
@@ -0,0 +1,131 @@
+use std;
+use std::collections::HashMap;
+
+use ron;
+use serde;
+use serde_derive;
+use serde_json;
+use serde_pickle;
+use serde_yaml;
+use toml;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: u32,
+    z: [i32; 3],
+    abc: f64,
+    s: String,
+    nu1: Option<bool>,
+    nu2: Option<bool>,
+    bo: bool,
+    map: HashMap<String, i32>,
+}
+
+fn test_json(p: &Point) {
+    println!("json");
+    let s = serde_json::to_string(p);
+    if s.is_err() {
+        println!("serialized\n{}", s.err().unwrap());
+        return;
+    }
+    let s = s.unwrap();
+    println!("serialized\n{}", s);
+    let d = serde_json::from_str::<Point>(&s);
+    if d.is_ok() {
+        println!("deserialized\n{:?}", d.unwrap());
+    } else {
+        println!("deserialized\n{}", d.err().unwrap());
+    }
+}
+
+fn test_yaml(p: &Point) {
+    println!("yaml");
+    let s = serde_yaml::to_string(p);
+    if s.is_err() {
+        println!("serialized\n{}", s.err().unwrap());
+        return;
+    }
+    let s = s.unwrap();
+    println!("serialized\n{}", s);
+    let d = serde_yaml::from_str::<Point>(&s);
+    if d.is_ok() {
+        println!("deserialized\n{:?}", d.unwrap());
+    } else {
+        println!("deserialized\n{}", d.err().unwrap());
+    }
+}
+
+fn test_toml(p: &Point) {
+    println!("toml");
+    let s = toml::to_string(p);
+    if s.is_err() {
+        println!("serialized\n{}", s.err().unwrap());
+        return;
+    }
+    let s = s.unwrap();
+    println!("serialized\n{}", s);
+    let d = toml::from_str::<Point>(&s);
+    if d.is_ok() {
+        println!("deserialized\n{:?}", d.unwrap());
+    } else {
+        println!("deserialized\n{}", d.err().unwrap());
+    }
+}
+
+fn test_pickle(p: &Point) {
+    println!("pickle");
+    let s = serde_pickle::to_vec(p, true);
+    if s.is_err() {
+        println!("serialized\n{}", s.err().unwrap());
+        return;
+    }
+    let s = s.unwrap();
+    println!("serialized\n{:?}", s);
+    let d = serde_pickle::from_slice::<Point>(&s);
+    if d.is_ok() {
+        println!("deserialized\n{:?}", d.unwrap());
+    } else {
+        println!("deserialized\n{}", d.err().unwrap());
+    }
+}
+
+fn test_ron(p: &Point) {
+    println!("ron");
+    let s = ron::ser::to_string(p);
+    if s.is_err() {
+        println!("serialized\n{}", s.err().unwrap());
+        return;
+    }
+    let s = s.unwrap();
+    println!("serialized\n{}", s);
+    let d = ron::de::from_str::<Point>(&s);
+    if d.is_ok() {
+        println!("deserialized\n{:?}", d.unwrap());
+    } else {
+        println!("deserialized\n{}", d.err().unwrap());
+    }
+}
+
+pub fn run() {
+    let mut point = Point {
+        x: 1,
+        y: std::u32::MAX,
+        z: [0, 1, 2],
+        abc: 0.12345678901234,
+        s: r#"abcあいう🍣🍺
+        "エスケープ"''"""#
+            .to_string(),
+        nu1: None,
+        nu2: Some(false),
+        bo: false,
+        map: HashMap::new(),
+    };
+    point.map.insert("key1".to_string(), 0);
+    point.map.insert("key2".to_string(), -1);
+    test_json(&point);
+    test_yaml(&point);
+    test_toml(&point);
+    test_pickle(&point);
+    test_ron(&point);
+}
