3b39f9300f9c2916fd30523502f1f2c4f3d9622b
RustyRobot <rustfmtbot@yandex.ru>
2018-12-03 00:37:40 +0300

    rustyrobot formatting

--- src/pf.rs
+++ src/pf.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
+use rmp_serde;
 use ron;
 use serde_json;
-use rmp_serde;
 use test;
 
 fn ser_non_string_key_map() {
@@ -23,7 +23,7 @@ fn ser_non_string_key_map() {
 fn size_test() {
     let mut map = HashMap::new();
     for i in 0..1000 {
-        map.insert([i, i + 1, i + 2, i+ 3 + i + 4 + i + 5], i);
+        map.insert([i, i + 1, i + 2, i + 3 + i + 4 + i + 5], i);
     }
     let s = rmp_serde::to_vec(&map).unwrap();
     println!("rmp={}", s.len());
@@ -35,7 +35,7 @@ fn size_test() {
 fn bench_rmp(b: &mut test::Bencher) {
     let mut map = HashMap::new();
     for i in 0..1000 {
-        map.insert([i, i + 1, i + 2, i+ 3, i + 4, i + 5], i);
+        map.insert([i, i + 1, i + 2, i + 3, i + 4, i + 5], i);
     }
     b.iter(move || rmp_serde::to_vec(&map).unwrap());
 }
@@ -44,7 +44,7 @@ fn bench_rmp(b: &mut test::Bencher) {
 fn bench_ron(b: &mut test::Bencher) {
     let mut map = HashMap::new();
     for i in 0..1000 {
-        map.insert([i, i + 1, i + 2, i+ 3, i + 4, i + 5], i);
+        map.insert([i, i + 1, i + 2, i + 3, i + 4, i + 5], i);
     }
     b.iter(move || ron::ser::to_string(&map).unwrap());
 }
